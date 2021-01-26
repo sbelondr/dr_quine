1a808413f5ee5ba4fb128f95c35bd124c1e70c27
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2021-01-26 11:42:45 +0100

    add new file and decrement for Sully: ok

--- asm/srcs/Sully.s
+++ asm/srcs/Sully.s
@@ -1,10 +1,10 @@
 section .text
 global _main
 extern _dprintf
-extern _printf
 extern _sprintf
 extern _setenv
 extern _getenv
+extern _system
 default rel
 
 section .text
@@ -15,84 +15,68 @@ section .text
 _main:
 push rbp
 
-;push rax
-
-_nameFile:
-lea rdi,[rel testt]
-lea rsi, [rel father]
-;pop rax
-;lea rdx, [rel father]
-mov rax, 0
-call _sprintf
-push rax
-call openFile
-ret
-
-openFile:
-;pop rax
-lea rdi,[rel father]
-mov rsi,1538
-mov rdx,0666o
-mov rax,0x2000005
-syscall
-mov [fd],rax
-xor rax,rax
-push rax
-call write_in_file
-ret
-
-write_in_file:
-mov rdi,1
-lea rsi,[rel father]
-mov rdx,0
-call _dprintf
-pop rax
-push rax
-call exit
+call _compil_and_launch
 ret
 
 checkI:
 lea rdi, [rel father]
 call _getenv
 lea r8, [rel rax]
-pop rbp
-push rbp
 mov rax, i
-push rax
+mov [ii], rax
 cmp r8,0
-;je _display
+je _nameFile
 call inc_i
 ret
 
 inc_i:
-pop rax
+mov rax, i
 dec rax
-push rax
-;call _display
+mov [ii], rax
+call _nameFile
 ret
 
-pop rbp
-push rbp
+_nameFile:
+lea rdi,[rel testt]
+lea rsi, [rel form]
+mov rdx, [ii]
+xor rax,rax
+call _sprintf
+push rax
+call openFile
+ret
 
+openFile:
+lea rdi,[rel testt]
+xor rax,rax
+mov rsi,1538
+mov rdx,0666o
+mov rax,0x2000005
+syscall
+mov [fd],rax
+call write_in_file
+ret
+
+write_in_file:
+mov rdi,rax
+lea rsi,[rel form]
+mov rdx,i
+xor rax,rax
+push rax
+call _dprintf
+pop rax
+push rax
+call _compil_and_launch
+ret
+
+_compil_and_launch:
+lea rdi, [rel testSys]
+call _system
+xor rax,rax
 call exit
-
-;lea rdi, [rel form]
-;lea rsi, [rel testt]
-;mov rdx, 0
-;xor rax,rax
-;call _printf
-;pop rbp
-;mov rax, 0
-;ret
-;ret
-
-
-call exit
+ret
 
 exit:
-;xor rdi,rdi
-;xor rsi,rsi
-;xor rax,rax
 mov rax,0x2000001
 mov rdi,0
 syscall
@@ -100,9 +84,11 @@ ret
 
 section .data
 fd:dq 0
+ii:dq 0
 father:db"father",0
 i:equ 5
 f:db"Sully_X.s",0
 format:db"%d",0
-form:db"Sully_%s.s",0
+form:db"Sully_%d.s",0
 testt:times 512 db 0
+testSys:db"ls -la",0
