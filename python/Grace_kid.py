915593c3a64141e5d0f72a78418b08f546c7ef78
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2021-03-30 05:40:30 +0200

    Sully: ok

--- python/Grace_kid.py
+++
@@ -1,4 +0,0 @@
-F='Grace_kid.py'
-A="F='Grace_kid.py'\nA=%r\nLA=lambda:[(_.write(A%%A), _.close()) for _ in [open(F,'w')]]\nLA()\n"
-LA=lambda:[(_.write(A%A), _.close()) for _ in [open(F,'w')]]
-LA()
--- python/srcs/Sully.py
+++ python/srcs/Sully.py
@@ -1,10 +1,10 @@
 import os
 x=5
-F='Sully_'+str(x)+'.py'
 if os.getenv('SULLY'):
   x-=1
-PR="import os\nx=%d\nF='Sully_'+str(x)+'.py'\nif os.getenv('SULLY'):\n  x-=1\nPR=%r\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nW()\nif x>0:\n  os.system('SULLY=1 python '+F)\n"
+F='Sully_'+str(x)+'.py'
 W=lambda:[(_.write(PR%(x,PR)), _.close()) for _ in [open(F, 'w')]]
+PR="import os\nx=%d\nif os.getenv('SULLY'):\n  x-=1\nF='Sully_'+str(x)+'.py'\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nPR=%r\nW()\nif x>0:\n  os.system('SULLY=1 python '+F)\n"
 W()
 if x>0:
   os.system('SULLY=1 python '+F)
