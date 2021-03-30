9eec1b8a8ae43a355ac59bc9168b9c5c5591cfce
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2021-03-30 19:12:13 +0200

    fix Sully python

--- python/srcs/Sully.py
+++ python/srcs/Sully.py
@@ -2,9 +2,10 @@ import os
 x=5
 if os.getenv('SULLY'):
   x-=1
+if x<0:
+  exit(0)
 F='Sully_'+str(x)+'.py'
 W=lambda:[(_.write(PR%(x,PR)), _.close()) for _ in [open(F, 'w')]]
-PR="import os\nx=%d\nif os.getenv('SULLY'):\n  x-=1\nF='Sully_'+str(x)+'.py'\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nPR=%r\nW()\nif x>0:\n  os.system('SULLY=1 python '+F)\n"
+PR="import os\nx=%d\nif os.getenv('SULLY'):\n  x-=1\nif x<0:\n  exit(0)\nF='Sully_'+str(x)+'.py'\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nPR=%r\nW()\nos.system('SULLY=1 python '+F)\n"
 W()
-if x>0:
-  os.system('SULLY=1 python '+F)
+os.system('SULLY=1 python '+F)
