e30d7af3f8d8d4288026656d76db5d147be25d0d
Samuel BELONDRADE <samuelbelondrade@outlook.fr>
2021-04-10 11:23:16 +0200

    fix error python for linux | call ft_test for Colleen.c

--- C/srcs/Colleen.c
+++ C/srcs/Colleen.c
@@ -19,7 +19,7 @@ char *l2="return;%1$c}%1$c%1$cint%1$cmain(void)%1$c{%1$c/*%1$c   comment%1$c*/%1
 char *l4="char *l%c=%c%s%c;%cchar *l%c=%c%s%c;%c";
 char *l5="char n='%cn', q='%c', b='%c%c';%c";
 char *l6="printf(";
-char *l7="%sl1,n);%sl2,n);%c";
+char *l7="%sl1,n);%sl2,n);ft_test();%c";
 char *l9="char *l%c=%c%s%c;%cchar *l%c=%c%s%c;%cchar *l%c=%c%s%c;%c";
 char *la="%sl9,'1',q,l1,q,n,'2',q,l2,q,n,'4',q,l4,q,n);%c";
 char *lb="%sl4,'5',q,l5,q,n,'6',q,l6,q,n);%sl4,'7',q,l7,q,n,'9',q,l9,q,n);%c";
@@ -27,7 +27,7 @@ char *ld="%sl9,'a',q,la,q,n,'b',q,lb,q,n,'d',q,ld,q,n);%c%sl4,'e',q,le,q,n,'f',q
 char *le="%1$sl5,b,q,b,b,n);%2$c%1$sl7,l6,l6,n);%1$sla,l6,n);%1$slb,l6,l6,n);%2$c";
 char *lf="%1$sld,l6,n,l6);%1$sle,l6,n);%1$slf,l6,n);%2$creturn 0;%2$c}%2$c";
 char n='\n', q='"', b='\\';
-printf(l1,n);printf(l2,n);
+printf(l1,n);printf(l2,n);ft_test();
 printf(l9,'1',q,l1,q,n,'2',q,l2,q,n,'4',q,l4,q,n);
 printf(l4,'5',q,l5,q,n,'6',q,l6,q,n);printf(l4,'7',q,l7,q,n,'9',q,l9,q,n);
 printf(l9,'a',q,la,q,n,'b',q,lb,q,n,'d',q,ld,q,n);
--- python/srcs/Sully.py
+++ python/srcs/Sully.py
@@ -6,6 +6,6 @@ if x<0:
   exit(0)
 F='Sully_'+str(x)+'.py'
 W=lambda:[(_.write(PR%(x,PR)), _.close()) for _ in [open(F, 'w')]]
-PR="import os\nx=%d\nif os.getenv('SULLY'):\n  x-=1\nif x<0:\n  exit(0)\nF='Sully_'+str(x)+'.py'\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nPR=%r\nW()\nos.system('SULLY=1 python '+F)\n"
+PR="import os\nx=%d\nif os.getenv('SULLY'):\n  x-=1\nif x<0:\n  exit(0)\nF='Sully_'+str(x)+'.py'\nW=lambda:[(_.write(PR%%(x,PR)), _.close()) for _ in [open(F, 'w')]]\nPR=%r\nW()\nos.system('SULLY=1 python3.8 '+F)\n"
 W()
-os.system('SULLY=1 python '+F)
+os.system('SULLY=1 python3.8 '+F)
